6538ceb9f95f689beb19e6500d84998b26d2caac
ayushthakur747 <itsathakur30@gmail.com>
2020-08-11 22:29:00 +0530

    deploy news app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
+import Particles from 'react-particles-js';
 
-function App() {
+import NewsCards from './Component/NewsCards/NewsCards.js';
+
+const alankey= '3e51a20fe72f60cb0955b473e4eab2742e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const particlesOptions = {
+  
+  particles: {
+    number: {
+      value: 80,
+      density:{
+        enable:true,
+        value_area:900 
+      }
+    }
+  }
+
+}
+
+
+const App = () => {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({command, articles, number}) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          
+        }else if(command === 'highlight'){
+            setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 )
+
+        }else if(command === 'open'){
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}) : number;
+          const article = articles[parsedNumber - 1] ;
+
+          if(parsedNumber > 20){
+            alanBtn().playText('please try again.')
+          }else if(article){
+            window.open(article.url,'blank');
+            alanBtn().playText('opening...');
+
+          }          
+        }
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'   
+            params={particlesOptions}
+          />
+      <h1>|| Alan AI News Application ||</h1>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </div>
   );
 }
