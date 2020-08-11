1ef2e0ee12e4fbbdaaa88c8ebe7bd781d2263a7e
ayushthakur747 <itsathakur30@gmail.com>
2020-08-11 22:39:52 +0530

    Updates

---
+++ precache-manifest.97657fa2c986ed0ace8b5fc6cc14753e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c0afd7980c1553837b486080de04db",
+    "url": "/ALAN_AI_NEWS_APP/index.html"
+  },
+  {
+    "revision": "f5ce6a71bca5c2a6f76d",
+    "url": "/ALAN_AI_NEWS_APP/static/css/main.9d81fa8d.chunk.css"
+  },
+  {
+    "revision": "ee5791a9b7859f63c259",
+    "url": "/ALAN_AI_NEWS_APP/static/js/2.699c0729.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/ALAN_AI_NEWS_APP/static/js/2.699c0729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ce6a71bca5c2a6f76d",
+    "url": "/ALAN_AI_NEWS_APP/static/js/main.48b09a0f.chunk.js"
+  },
+  {
+    "revision": "27055e71a9979888cfee",
+    "url": "/ALAN_AI_NEWS_APP/static/js/runtime-main.c663d5fa.js"
+  }
+]);
